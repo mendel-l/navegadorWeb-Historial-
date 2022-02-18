d2707ce113869ceb47599a97f1e0fee72dc4e447
Usuario <Usuario@DESKTOP-RESB091>
2022-02-18 08:56:34 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace navegadorWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonIR = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navegarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siguienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anteriorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addressBar = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonIR
+            // 
+            this.buttonIR.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonIR.Location = new System.Drawing.Point(628, 3);
+            this.buttonIR.Name = "buttonIR";
+            this.buttonIR.Size = new System.Drawing.Size(81, 26);
+            this.buttonIR.TabIndex = 0;
+            this.buttonIR.Text = "IR";
+            this.buttonIR.UseVisualStyleBackColor = true;
+            this.buttonIR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navegarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(712, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navegarToolStripMenuItem
+            // 
+            this.navegarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.siguienteToolStripMenuItem,
+            this.anteriorToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.navegarToolStripMenuItem.Name = "navegarToolStripMenuItem";
+            this.navegarToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.navegarToolStripMenuItem.Text = "Navegar";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.inicioToolStripMenuItem.Text = "inicio";
+            this.inicioToolStripMenuItem.Click += new System.EventHandler(this.inicioToolStripMenuItem_Click);
+            // 
+            // siguienteToolStripMenuItem
+            // 
+            this.siguienteToolStripMenuItem.Name = "siguienteToolStripMenuItem";
+            this.siguienteToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.siguienteToolStripMenuItem.Text = "siguiente";
+            this.siguienteToolStripMenuItem.Click += new System.EventHandler(this.siguienteToolStripMenuItem_Click);
+            // 
+            // anteriorToolStripMenuItem
+            // 
+            this.anteriorToolStripMenuItem.Name = "anteriorToolStripMenuItem";
+            this.anteriorToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.anteriorToolStripMenuItem.Text = "anterior";
+            this.anteriorToolStripMenuItem.Click += new System.EventHandler(this.anteriorToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.salirToolStripMenuItem.Text = "salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // addressBar
+            // 
+            this.addressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressBar.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.addressBar.FormattingEnabled = true;
+            this.addressBar.Location = new System.Drawing.Point(3, 3);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(619, 28);
+            this.addressBar.TabIndex = 4;
+            this.addressBar.SelectedIndexChanged += new System.EventHandler(this.addressBar_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 87F));
+            this.tableLayoutPanel1.Controls.Add(this.addressBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonIR, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.webView21, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(712, 464);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // webView21
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.webView21, 2);
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(2, 34);
+            this.webView21.Margin = new System.Windows.Forms.Padding(2);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(708, 428);
+            this.webView21.TabIndex = 5;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonIR;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 488);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIR;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navegarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siguienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anteriorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ComboBox addressBar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace navegadorWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webView21.NavigationStarting += WebView21_NavigationStarting;
+            InitializeAsync();
+        }
+
+        private void WebView21_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+        }
+
+        async void InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.WebMessageReceived += ActualizarBarraDireccion;
+
+            await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+
+        }
+
+        void ActualizarBarraDireccion(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            addressBar.Text = uri;
+            //webView21.CoreWebView2.PostWebMessageAsString(uri);
+        }
+
+        private void Read(String pathIn)
+        {
+            FileStream stream = new FileStream(pathIn, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() < -1)
+            {
+                string texto = reader.ReadLine();
+                addressBar.Items.Add(addressBar.Text);
+            }
+            reader.Close();
+        }
+
+        private void Save(string nombreArchivo, string texto)
+        {
+            //FileStream stream = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new /*StreamWriter(stream);*/StreamWriter(nombreArchivo, true);
+
+            writer.WriteLine(texto);
+            writer.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoANavegar = addressBar.Text;
+
+            Save(@"historialNavegacion.txt", addressBar.Text);
+            //el txt. se guarda en la ubicacion donde esta creado el archivo en la carpeta bin/Debug en esta ultima se encuentra el archivo txt.
+
+            if (textoANavegar.StartsWith("https://www.google.com/search?q=") == false)
+            {
+                addressBar.Items.Add("https://www.google.com/search?q=" + addressBar.Text);
+            }
+            //addressBar.Items.Add(addressBar.Text);
+
+            // es una direccion
+            if (textoANavegar.Contains(".com") || textoANavegar.Contains("http") ||
+                textoANavegar.Contains("www"))
+            {
+                if (textoANavegar.StartsWith("https://") == false
+                    && textoANavegar.StartsWith("http://") == false)
+                {
+                    textoANavegar = "https://" + textoANavegar;
+                }
+
+                webView21.CoreWebView2.Navigate(textoANavegar);
+            }
+            // es una palabra
+            else
+            {
+                webView21.CoreWebView2.Navigate("https://www.google.com/search?q=" + textoANavegar);
+            }
+
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void siguienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.GoForward();
+        }
+
+        private void anteriorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.GoBack();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read(@"historialNavegacion.txt");
+            //Read(@"Y:\A LA UNI cap3\PROGRAMACION\Laboratorios\Laboratorio #3\navegadorHistorial\navegadorWeb\bin\Debug\historialNavegacion.txt");
+        }
+
+        private void addressBar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Read(@"historialNavegacion.txt");
+            //Read(@"Y:\A LA UNI cap3\PROGRAMACION\Laboratorios\Laboratorio #3\navegadorHistorial\navegadorWeb\bin\Debug\historialNavegacion.txt");
+        }
+    }
+}
